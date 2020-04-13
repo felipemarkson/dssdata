f3b46453d43ab70f9d2036701cec6abae1562fa4
Felipe Markson <fmarkson@outlook.com>
2020-04-13 15:48:35 -0300

    Modularização do projeto

---
+++ powerflow/line_tools.py
@@ -0,0 +1,100 @@
+import pandas as pd
+from .systemclass import SystemClass
+
+from .formatters import (
+    __identify_ph_config,
+    __get_mag_vanish,
+    __get_ang_vanish)
+
+
+def get_line_infos(distSys: SystemClass, lines_names: list):
+
+    def vanish_line_infos(bus_raw: list, current_raw: list):
+        bus_name = bus_raw[0]
+        phs_raw = list(map(lambda bus: int(bus), bus_raw[1:]))
+        if phs_raw == []:
+            phs_raw = [1, 2, 3]
+        phs = __identify_ph_config(phs_raw)
+        currents_mag = __get_mag_vanish(phs_raw, current_raw)
+        currents_ang = __get_ang_vanish(phs_raw, current_raw)
+
+        return (bus_name, phs, currents_mag, currents_ang)
+
+    data_list = []
+
+    for line_name in lines_names:
+        distSys.dss.Lines.Name(line_name)
+        losses = distSys.dss.CktElement.Losses()
+        normalAmps = distSys.dss.CktElement.NormalAmps()
+        emergAmps = distSys.dss.CktElement.EmergAmps()
+        currents_raw = distSys.dss.CktElement.CurrentsMagAng()
+        currents_raw_bus1 = currents_raw[:int(len(currents_raw)/2)]
+        currents_raw_bus2 = currents_raw[int(len(currents_raw)/2):]
+
+        bus_raw = distSys.dss.Lines.Bus1().split('.')
+        (bus_name1, phs1,
+            currents_mag1, currents_ang1) = vanish_line_infos(
+            bus_raw, currents_raw_bus1)
+        bus_raw = distSys.dss.Lines.Bus2().split('.')
+        (bus_name2, phs2,
+            currents_mag2, currents_ang2) = vanish_line_infos(
+            bus_raw, currents_raw_bus2)
+
+        currents_mag1_calc = currents_mag1.copy()
+        while None in currents_mag1_calc:
+            currents_mag1_calc.remove(None)
+        currents_mag2_calc = currents_mag2.copy()
+        while None in currents_mag2_calc:
+            currents_mag2_calc.remove(None)
+
+        data = {
+            'name': line_name,
+            'bus1': bus_name1,
+            'ph_bus1': phs1,
+            'bus2': bus_name2,
+            'ph_bus2': phs2,
+            'I(A)_bus1_ph_a': round(currents_mag1[0], 3)
+            if currents_mag1[0] is not None else None,
+            'I(A)_bus1_ph_b': round(currents_mag1[1], 3)
+            if currents_mag1[1] is not None else None,
+            'I(A)_bus1_ph_c': round(currents_mag1[2], 3)
+            if currents_mag1[2] is not None else None,
+            'I(A)_bus2_ph_a': round(currents_mag2[0], 3)
+            if currents_mag2[0] is not None else None,
+            'I(A)_bus2_ph_b': round(currents_mag2[1], 3)
+            if currents_mag2[1] is not None else None,
+            'I(A)_bus2_ph_c': round(currents_mag2[2], 3)
+            if currents_mag2[2] is not None else None,
+            'ang_bus1_ph_a': round(currents_ang1[0], 2)
+            if currents_ang1[0] is not None else None,
+            'ang_bus1_ph_b': round(currents_ang1[1], 2)
+            if currents_ang1[1] is not None else None,
+            'ang_bus1_ph_c': round(currents_ang1[2], 2)
+            if currents_ang1[2] is not None else None,
+            'ang_bus2_ph_a': round(currents_ang2[0], 2)
+            if currents_ang2[0] is not None else None,
+            'ang_bus2_ph_b': round(currents_ang2[1], 2)
+            if currents_ang2[1] is not None else None,
+            'ang_bus2_ph_c': round(currents_ang2[2], 2)
+            if currents_ang2[2] is not None else None,
+            'kw_losses': round(losses[0]/1000, 3),
+            'kvar_losses': round(losses[1]/1000, 3),
+            'emergAmps': round(emergAmps, 3),
+            'normAmps': round(normalAmps, 3),
+            'perc_NormAmps': round(
+                max(
+                    currents_mag1_calc + currents_mag2_calc)/normalAmps, 3
+            ),
+            'perc_EmergAmps': round(
+                max(
+                    currents_mag1_calc + currents_mag2_calc)/emergAmps, 3
+            ),
+        }
+        data_list.append(data)
+
+    return pd.DataFrame(data_list)
+
+
+def get_all_line_infos(distSys: SystemClass):
+    line_names = distSys.get_all_lines_names()
+    return get_line_infos(distSys, line_names)
---
+++ powerflow/voltage_tools.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from .systemclass import SystemClass
+
+
+from .tools import __verify_bus_list, __get_bus_ang
+from .tools import __get_bus_v_pu, __get_bus_ph
+from .formatters import __identify_ph_config
+
+
+def get_all_v_pu_angle(distSys: SystemClass):
+    buses = distSys.get_all_bus_names()
+    return get_bus_v_pu_ang(distSys, buses)
+
+
+def get_bus_v_pu_ang(distSys: SystemClass, buses: list):
+    list_verify = __verify_bus_list(distSys, buses)
+    if not all(list_verify):
+        for (verify, bus) in zip(list_verify, buses):
+            if not verify:
+                raise Exception(
+                    f"A barra {bus} não está declarada no sistema")
+
+    v_pu_list = []
+    ang_list = []
+    ph_list = []
+
+    for bus in buses:
+        ang_list.append(__get_bus_ang(distSys, bus))
+        v_pu_list.append(__get_bus_v_pu(distSys, bus))
+        ph = __get_bus_ph(distSys, bus)
+        ph_config = __identify_ph_config(ph)
+        ph_list.append(ph_config)
+
+    df_bus_names = pd.DataFrame(buses, columns=['bus_names'])
+    df_v_pu = pd.DataFrame(v_pu_list, columns=[
+        'v_pu_a', 'v_pu_b', 'v_pu_c'])
+    df_ang = pd.DataFrame(ang_list, columns=['ang_a', 'ang_b', 'ang_c'])
+    df_ph = pd.DataFrame(ph_list, columns=['phases'])
+
+    result = pd.concat(
+        [df_bus_names, df_v_pu, df_ang, df_ph], axis=1, sort=False)
+    return result
