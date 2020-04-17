850ac2ccf4621913f7e263332199f7e15407a4a0
Felipe Markson <fmarkson@outlook.com>
2020-04-17 11:04:47 -0300

    fix decorator bug

--- powerflow/decorators.py
+++ powerflow/decorators.py
@@ -1,7 +1,7 @@
 def pf_tools(func):
-    def wrapper(distSys, *args):
+    def wrapper(distSys, *args, **kwds):
         if distSys.dss.Circuit.Name() != distSys.get_name():
             distSys.compile()
             # distSys.dss.Circuit.Enable(distSys.get_name())
-        return func(distSys, *args)
+        return func(distSys, *args, **kwds)
     return wrapper
