0f11c39d74efaae72dd3217855a87e3353b38612
Felipe Markson <fmarkson@outlook.com>
2020-04-24 11:47:35 -0300

    Melhoria no reg_tools

--- powerflow/reg_tools.py
+++ powerflow/reg_tools.py
@@ -3,6 +3,7 @@ from .decorators import pf_tools
 from .formatters import __check_elements
 import pandas
 from typing import List, Tuple
+from operator import add
 
 
 @pf_tools
@@ -33,17 +34,7 @@ def get_reg_tap_number(
     """
 
     def get_one(reg_name: str) -> Tuple[str, int]:
-        """Função auxiliar para map.
-
-        Arguments:
-            reg_name {str} -- Nome do regulador
-
-        Returns:
-            Tuple[str, int] -- (Nome do regulador, Número de tap)
-        """
-
         distSys.dss.RegControls.Name(reg_name)
-
         return (reg_name, int(distSys.dss.RegControls.TapNumber()))
 
     __check_elements(reg_names, distSys.get_all_regs_names())
@@ -53,32 +44,20 @@ def get_reg_tap_number(
     )
 
 
-def __get_taps_changes(
-    distSys: SystemClass, reg_names: List[str]
-) -> pandas.DataFrame:
-    """
-    TODO: fazer o taps changes por step.
-    """
-    pass
-
-
 def get_taps_changes(tapDataFrame: pandas.DataFrame) -> pandas.DataFrame:
-
-    def changes_of_one_step(first: list, second: list) -> list:
+    def calc_one_step_chgs(first: list, second: list) -> list:
         return map(lambda data1, data2: abs(data1 - data2), first, second)
 
     def sum_changes_taps(list_df: List[list]) -> list:
         first = list_df[0]
         second = list_df[1]
         if len(list_df) == 2:
-            return changes_of_one_step(first, second)
+            return calc_one_step_chgs(first, second)
         else:
-            return list(
-                map(
-                    lambda data1, data2: data1 + data2,
-                    changes_of_one_step(first, second),
-                    sum_changes_taps(list_df[1:]),
-                )
+            return map(
+                add,
+                calc_one_step_chgs(first, second),
+                sum_changes_taps(list_df[1:]),
             )
 
     iter_by_step = tapDataFrame.groupby(["step"])
@@ -89,12 +68,6 @@ def get_taps_changes(tapDataFrame: pandas.DataFrame) -> pandas.DataFrame:
     )[0]
 
     return pandas.DataFrame(
-        data=list(
-            map(
-                lambda name, changes: (name, changes),
-                reg_names,
-                sum_changes_taps(list_df),
-            )
-        ),
+        data=list(zip(reg_names, sum_changes_taps(list_df))),
         columns=["reg_name", "number_changes_tap"],
     )
